f0e89ed5d324600313aa0c9df2905c8c4d0e609f
AbhiShek Aggarwal <warabhishek@gmail.com>
2019-08-26 17:40:14 -0400

    sm8150-common: Add custom init to set proper variants
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I04f09a14cf41c89b3135c53e3089d5c3e703aae0
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,6 +110,10 @@ TARGET_USES_FOD_ZPOS := true
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_cepheus
+TARGET_RECOVERY_DEVICE_MODULES := libinit_cepheus
+
 # Media
 TARGET_USES_ION := true
 TARGET_DISABLED_UBWC := true
