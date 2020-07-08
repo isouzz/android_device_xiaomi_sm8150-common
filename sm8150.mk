42693647f1cbc644a254f845b9124fe7488c29f1
Vinoth Jayaram <c_vinoja@codeaurora.org>
2020-07-08 12:14:30 +0530

    sm8150-common: sm6150: Updated Display HALs
    Removed HIDL Light and configstore Hal
    Enabled Light AIDL HAL
    
    Change-Id: Ib705e9e59c5409f30b50499ea0a33c4c550d1d1f

--- sm8150.mk
+++ sm8150.mk
@@ -197,7 +197,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm8150
+    android.hardware.lights-service.qti
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
